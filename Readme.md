44cb276e3f03339aadd19d30d130579dd14639ca
Matthias Lüneberg <mail@matthias-lueneberg.de>
2020-08-27 17:46:23 +0200

    Add basic domain objects

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Rock-Paper-Scissor
+
+This is a basic implementation of the game [Rock-Paper-Scissor](https://en.wikipedia.org/wiki/Rock-paper-scissors) 
+in Kotlin.
+
+## Build the code
+
+    ./gradlew cK
---
+++ src/main/kotlin/it/lueneberg/rsp/Domain.kt
@@ -0,0 +1,29 @@
+package it.lueneberg.rsp
+
+enum class Result {
+    WIN, DRAW, LOSE
+}
+
+enum class Action {
+    ROCK, PAPER, SCISSOR;
+
+    fun evaluate(other: Action): Result {
+        return when (this) {
+            ROCK -> when (other) {
+                ROCK -> Result.DRAW
+                PAPER -> Result.LOSE
+                SCISSOR -> Result.WIN
+            }
+            PAPER -> when (other) {
+                ROCK -> Result.WIN
+                PAPER -> Result.DRAW
+                SCISSOR -> Result.LOSE
+            }
+            SCISSOR -> when (other) {
+                ROCK -> Result.LOSE
+                PAPER -> Result.WIN
+                SCISSOR -> Result.DRAW
+            }
+        }
+    }
+}
